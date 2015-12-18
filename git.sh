504d0b545a06c8b731b175874c888c7e1dd08836
william-mi <william@quarahtk.org>
2015-12-18 16:47:23 +0100

    Update	git

--- git.sh
+++ git.sh
@@ -1,27 +1,30 @@
 #!/bin/sh
+read choix
 
+case $choix in
+	1) 	
+		printf "Entrez votre pseudo Git :"
+		read nom		
+		git config user.name "$nom"
+		printf "Entrez votre email (compte Github) :"
+		read email
+		git config --global user.email "$email"
+		printf "Entrez un proxy si necessaire :"
+		read proxy
+		if [ "$proxy" != "" ]; then 
+		git config --global http.proxy $proxy
+		fi
+	;;
+	
+	2)
+		cd ~
+		mkdir ATB
+		git init ATB
+		cd ATB
+		git remote add ATB https://github.com/AdminToolsBox/ATB.git
+		git pull ATB master
 
-
-case
-printf "Entrez votre pseudo (Github) :"
-read nom
-git config user.name "$nom"
-printf "Entrez votre email (compte Github) :"
-read email
-git config --global user.email "$email"
-printf "Entrez un proxy si necessaire :"
-read proxy
-if [ "$proxy" != "" ]; then 
-git config --global http.proxy $proxy
-fi
-
-
-#cd ~
-#mkdir ATB
-#git init ATB
-#cd ATB
-#git remote add ATB https://github.com/AdminToolsBox/ATB.git
-#git pull ATB master
+esac
 
 
 
